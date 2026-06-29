d9b7993914924fe83c3625d6d8a07cd74eb1944c
cappuccino <rianaga80@gmail.com>
2026-06-29 09:41:01 +0700

    Update yoru.sh

--- yoru.sh
+++ yoru.sh
@@ -25,7 +25,7 @@ KIMG="$OUTDIR/Image.gz"
 export PATH="$ROOTDIR/weebx-clang/bin:$PATH"
 
 # ================= INFO =================
-KERNEL_NAME="PhroLova"
+KERNEL_NAME="Evergreen-Kernel-V1.0-[Perf+KSU]"
 DEVICE="vince"
 
 # =============== DATE (WIB) ===============
